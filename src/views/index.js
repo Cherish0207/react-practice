109d0d16d2011e27bacd5818a5fb27df57e9dbfb
Cherish0207 <cheshire_1207@foxmail.com>
2021-09-01 14:22:31 +0800

    配置react-loadable路由懒加载

--- src/views/index.js
+++ src/views/index.js
@@ -1,9 +1,30 @@
-import Article from "./article"
-import ArticleEdit from "./article/Edit"
-import Dashboard from "./dashboard"
-import Login from "./login"
-import Settings from "./settings"
-import Notfound from "./notfound"
+import Loadable from "react-loadable";
+import Loading from "../components/loading";
+// 需要将对外的普通组件需要进行懒加载
+const Article = Loadable({
+  loader: () => import("./article"),
+  loading: Loading,
+});
+const Dashboard = Loadable({
+  loader: () => import("./dashboard"),
+  loading: Loading,
+});
+const ArticleEdit = Loadable({
+  loader: () => import("./article/Edit"),
+  loading: Loading,
+});
+const Login = Loadable({
+  loader: () => import("./login"),
+  loading: Loading,
+});
+const Notfound = Loadable({
+  loader: () => import("./notfound"),
+  loading: Loading,
+});
+const Settings = Loadable({
+  loader: () => import("./settings"),
+  loading: Loading,
+});
 
 export {
   Article, ArticleEdit, Dashboard, Login, Settings, Notfound
