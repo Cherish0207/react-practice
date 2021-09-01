cae30000129a0aa9d68e7e34832ded41c9e3f869
Cherish0207 <cheshire_1207@foxmail.com>
2021-09-01 16:23:57 +0800

    less

--- src/components/frame/index.js
+++ src/components/frame/index.js
@@ -1,24 +1,32 @@
-import { Layout, Menu } from "antd";
+import { Layout, Menu, Breadcrumb } from "antd";
 import { Component } from "react";
 import PropTypes from "prop-types";
-import "antd/dist/antd.css"
+
 const { Header, Content, Footer } = Layout;
+import Logo from "./logo.png"
+import "./index.less"
 
 export default class Index extends Component {
   render () {
     return (<Layout className="layout">
-      <Header>
-        <div className="logo" />
-        <Menu theme="dark" mode="horizontal" >
-          {new Array(2).fill(null)
-            .map((_, index) => {
-              const key = index + 1;
-              return <Menu.Item key={key}>{`nav ${key}`}</Menu.Item>;
-            })}
-        </Menu>
+      <Header className="header qf-header">
+        <div className="logo" >
+          <img src={Logo}/>
+          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["2"]}>
+            {new Array(15).fill(null)
+              .map((_, index) => {
+                const key = index + 1;
+                return <Menu.Item key={key}>{`nav ${key}`}</Menu.Item>;
+              })}
+          </Menu>
+        </div>
       </Header>
       <Content style={{  padding: "0 24px", minHeight: 280  }}>
-        {this.props.children}
+        <Breadcrumb style={{ margin: "16px 0" }}>
+          <Breadcrumb.Item>Home</Breadcrumb.Item>
+          <Breadcrumb.Item>List</Breadcrumb.Item>
+          <Breadcrumb.Item>App</Breadcrumb.Item>
+        </Breadcrumb>{this.props.children}
       </Content>
       <Footer style={{ textAlign: "center" }}>Ant Design ©2018 Created by Ant UED</Footer>
     </Layout>)
